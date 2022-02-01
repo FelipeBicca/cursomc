a6e17989fc9c18fa57003d0f66a44c20614a302e
Felipe Bicca <felipedbicca@hotmail.com>
2022-02-01 16:46:22 -0300

    Endpoint /pedidos/{id} disponibilizados

---
+++ src/main/java/com/felipebicca/cursomc/resources/PedidoResource.java
@@ -0,0 +1,27 @@
+package com.felipebicca.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipebicca.cursomc.domain.Pedido;
+import com.felipebicca.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value = "/pedidos")
+public class PedidoResource {
+
+	@Autowired
+	private PedidoService pedido;
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido obj = pedido.find(id);
+		return ResponseEntity.ok().body(obj);
+	
+
+	}
+}
