af3e49d12e758c4fcc45f62aa5931b58ab96e57a
Felipe Bicca <felipedbicca@hotmail.com>
2022-02-16 15:22:28 -0300

    Validação customizada do campo e-mail, email não repetido na inserção do cliente

--- src/main/java/com/felipebicca/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/felipebicca/cursomc/repositories/ClienteRepository.java
@@ -2,6 +2,7 @@ package com.felipebicca.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.felipebicca.cursomc.domain.Cliente;
 
@@ -9,4 +10,7 @@ import com.felipebicca.cursomc.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
+		
 }
