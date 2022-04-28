e4d7a6bc2cb95f26d49980be336f3ad6c286ebd3
Felipe Bicca <felipedbicca@hotmail.com>
2022-04-28 14:32:32 -0300

    Salvando os perfis na base de dados

--- src/main/java/com/felipebicca/cursomc/domain/Cliente.java
+++ src/main/java/com/felipebicca/cursomc/domain/Cliente.java
@@ -6,17 +6,20 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.felipebicca.cursomc.domain.enums.Perfil;
 import com.felipebicca.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -48,9 +51,15 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name = "telefone")
 	private Set<String> telefones = new HashSet<>();
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "perfis")
+	private Set<Integer> perfis = new HashSet<>();
+	
+	
 
 	public Cliente() {
-
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente, String senha) {
@@ -61,6 +70,7 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipoCliente = (tipoCliente == null) ? null : tipoCliente.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
@@ -133,6 +143,14 @@ public class Cliente implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	public Set<Perfil> getPerfis(){	
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
 	}
 
 	public void setTipoCliente(Integer tipoCliente) {
--- src/main/java/com/felipebicca/cursomc/services/DBService.java
+++ src/main/java/com/felipebicca/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.felipebicca.cursomc.domain.PagamentoComCartao;
 import com.felipebicca.cursomc.domain.Pedido;
 import com.felipebicca.cursomc.domain.Produto;
 import com.felipebicca.cursomc.domain.enums.EstadoPagamento;
+import com.felipebicca.cursomc.domain.enums.Perfil;
 import com.felipebicca.cursomc.domain.enums.TipoCliente;
 import com.felipebicca.cursomc.repositories.CategoriaRepository;
 import com.felipebicca.cursomc.repositories.CidadeRepository;
@@ -95,10 +96,20 @@ public class DBService {
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "felipedbicca@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("12345"));
+		Cliente cli2 = new Cliente(null, "Ana Costa", "felipedbicca@hotmail.com", "59435148050", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("12345"));
+		
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838391"));
+		cli2.getTelefones().addAll(Arrays.asList("32335025", "991254000"));
+		
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Av. Matos", "105", "Sala 800", "Centro", "98777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Rua Andradas", "79", null, "Centro", "96200030", cli2, c3);
+		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
@@ -146,8 +157,8 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
