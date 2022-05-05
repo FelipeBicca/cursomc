687c2c5b9190064d9385b56679db8b6251cd6e23
Felipe Bicca <felipedbicca@hotmail.com>
2022-05-05 14:30:34 -0300

    Enviando imagem via endpoint

--- src/main/java/com/felipebicca/cursomc/CursomcApplication.java
+++ src/main/java/com/felipebicca/cursomc/CursomcApplication.java
@@ -1,25 +1,18 @@
 package com.felipebicca.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.felipebicca.cursomc.services.S3Services;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
-	@Autowired
-	S3Services s3Services;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Services.uploadFile("C:\\Users\\Confidere\\Pictures\\Minhas Fotos\\PETS\\Pandora\\IMG_4391.JPG");
-		
+
 	}
 }
--- src/main/java/com/felipebicca/cursomc/services/ClienteService.java
+++ src/main/java/com/felipebicca/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.felipebicca.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.felipebicca.cursomc.domain.Cidade;
 import com.felipebicca.cursomc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 
 	@Autowired
 	private EnderecoRepository endRepo;
+	
+	@Autowired
+	private S3Services s3Services;
 
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -113,6 +118,10 @@ public class ClienteService {
 			cli.getTelefones().add(obj.getTelefone3());
 		}
 		return cli;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Services.uploadFile(multipartFile);
 	}
 
 }
